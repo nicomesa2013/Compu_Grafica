f4e94c99e4acf1a0c2f3c4e2e3e7db857c1e3a30
Nicolas Mesa <nicomesa2013@gmail.com>
2019-10-04 12:39:53 -0500

    juego movimiento en 2 direcciones, rivales implementados

--- juego001.py
+++ juego001.py
@@ -1,6 +1,14 @@
 import pygame 
+import random
 from grafica import *
 
+
+'''
+Rama : b1_2dir
+Autor: Nicolas Mesa Serna
+Descripcion: Movimiento en 2 direcciones rivales
+
+'''
 ANCHO = 640
 ALTO = 480
 
@@ -10,14 +18,41 @@ class Jugador(pygame.sprite.Sprite):
         self.image = pygame.Surface([40,50])
         self.image.fill(VERDE)
         self.rect = self.image.get_rect()
-        self.rect.x = 100
-        self.rect.y = 200
+        self.rect.x = 0
+        self.rect.y = ALTO - self.rect.height
         self.velx = 0
-        self.vely = 0
 
     def update(self):
         self.rect.x += self.velx
-        self.rect.y +=self.vely
+
+
+class Rival(pygame.sprite.Sprite):
+    def __init__(self):
+        '''Constructor rival'''
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([40,50])
+        self.image.fill(ROSADO)
+        self.rect = self.image.get_rect()
+        self.rect.x = 100
+        self.rect.y = 200
+        self.velx = 5
+        self.vely = 0
+        self.disparar = False
+        self.temp = random.randrange(150)
+
+    def Pos(self):
+        '''Retorna posicion del jugador'''
+        p = [self.rect.centerx,self.rect.y]
+        return p
+
+    def update(self):
+        '''Actualizar obejto'''
+        self.rect.x += self.velx
+        if self.rect.x > (ANCHO - self.rect.width):
+            self.velx = -5
+        if self.rect.x < 0:
+            self.velx = +5
+        self.temp-= 1        
 
 
 if __name__ == '__main__':
@@ -25,10 +60,23 @@ if __name__ == '__main__':
 
     pygame.init()
     pantalla = pygame.display.set_mode([ANCHO,ALTO])
+    
+    #Inicializar grupos
     jugadores = pygame.sprite.Group()
+    rivales  = pygame.sprite.Group()
+    
 
     j = Jugador()
     jugadores.add(j)
+
+
+    #Inicializar rivales
+    n = 10 
+    for i in range(n):
+        r = Rival()
+        r.rect.x = random.randrange(ANCHO)
+        r.rect.y = random.randrange(ALTO - 100)
+        rivales.add(r)
 
     reloj = pygame.time.Clock()
     fin = False
@@ -39,21 +87,21 @@ if __name__ == '__main__':
             if event.type == pygame.QUIT:
                 fin = True
             if event.type == pygame.KEYDOWN:
+                #Gestion de teclas
                 if event.key == pygame.K_RIGHT:
                     j.velx += 5
-                    j.vely = 0
+                        
                 if event.key == pygame.K_LEFT:
                     j.velx += -5
-                    j.vely = 0
-                if event.key == pygame.K_UP:
-                    j.velx = 0
-                    j.vely += 5
-                if event.key == pygame.K_DOWN:
-                    j.velx = 0
-                    j.vely += -5
+
+            if event.type == pygame.KEYUP:
+                #Objeto se detiene al liberar
+                j.velx = 0
 
 
         #Gestion de control
+
+        #Control de limites del jugador
         if j.rect.x < 0:
             j.rect.x = 0
             j.velx=0
@@ -66,11 +114,26 @@ if __name__ == '__main__':
         if j.rect.y > (ALTO - j.rect.height):
             j.rect.y = ALTO - j.rect.height
             j.vely=0
-        
+
+        #Gestion de rivales
+        for r in rivales:
+
+            if r in rivales:
+                if r.temp == 0:
+                    r.disparar = True
+            if r.disparar:
+                r.disparar = False
+                r.temp = random.randrange(150)
+
+
 
         #Gestion pantalla
         jugadores.update()
+        rivales.update()
+
+        #Despeglegar graficos
         pantalla.fill(NEGRO)
+        rivales.draw(pantalla)
         jugadores.draw(pantalla)
         pygame.display.flip()
         reloj.tick(60)
