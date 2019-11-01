c0bbb2893b62a75ced286daedf4d18db87974153
nicomesa2013 <nikomesa20@hotmail.com>
2019-11-01 00:04:40 -0500

    Sprites con movimiento aun hay error en colisiones, implementacion de lectura de mapa

---
+++ animacionSprite.py
@@ -0,0 +1,132 @@
+import pygame
+import random
+
+ANCHO = 640
+ALTO = 480
+VERDE=[0,255,0]
+BLANCO=[255,255,255]
+NEGRO=[0,0,0]
+ROJO=[255,0,0]
+
+def Recortar(imagen,a,b):
+    info = imagen.get_rect()
+    print info
+    c = info[2]/a
+    d = info[3]/b
+    m=[]
+    x = 0
+    while x < info[2]:
+        y = 0
+        ls=[]
+        while y < info[3]:
+            cuadro = imagen.subsurface(x,y,c,d)
+            ls.append(cuadro)
+            y+=d
+        m.append(ls)
+        x+=c
+    return m
+
+class Jugador(pygame.sprite.Sprite):
+    def __init__(self,m):
+        pygame.sprite.Sprite.__init__(self)
+        self.m=m
+        self.direccion=0
+        self.con=0
+        self.image=self.m[self.con][self.direccion]
+        self.rect=self.image.get_rect()
+        self.velx=0
+        self.vely=0
+
+    def update(self):
+        self.rect.x += self.velx
+        self.rect.y += self.vely
+        self.image=self.m[self.con][self.direccion]
+        if self.con < 2:
+            self.con +=1
+        else:
+            self.con = 0
+
+
+
+class Bloque(pygame.sprite.Sprite):
+    def __init__(self,pto):
+        pygame.sprite.Sprite.__init__(self)
+        self.image=pygame.Surface([60,60])
+        self.image.fill(BLANCO)
+        self.rect=self.image.get_rect()
+        self.rect.x=pto[0]
+        self.rect.y=pto[1]
+
+
+if __name__ == '__main__':
+    '''Programa principal'''
+    pygame.init()
+    pantalla = pygame.display.set_mode([ANCHO,ALTO])
+    jugadores=pygame.sprite.Group()
+    bloques=pygame.sprite.Group()
+
+
+    imagen = pygame.image.load('animals.png')
+    m = Recortar(imagen,12,8)
+    j=Jugador(m)
+    jugadores.add(j)
+
+    b=Bloque([300,200])
+    bloques.add(b)
+
+    fin = False
+    reloj=pygame.time.Clock()
+    while not fin:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                fin=True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    j.direccion=2
+                    j.velx=10
+                    j.vely=0
+                if event.key == pygame.K_LEFT:
+                    j.direccion=1
+                    j.velx=-10
+                    j.vely=0
+                if event.key == pygame.K_UP:
+                    j.direccion=3
+                    j.velx=0
+                    j.vely=-10
+                if event.key == pygame.K_DOWN:
+                    j.direccion=0
+                    j.velx=0
+                    j.vely=10
+            if event.type == pygame.KEYUP:
+                j.velx=0
+                j.vely=0
+
+        #colision
+        ls_col=pygame.sprite.spritecollide(j,bloques,False)
+        for b in ls_col:
+            if j.velx>0:
+                if j.rect.right > b.rect.left:
+                    j.rect.right = b.rect.left
+                    j.velx=0
+
+            if j.velx<0:
+                if j.rect.left < b.rect.right:
+                    j.rect.left = b.rect.right
+                    j.velx=0
+
+            if j.vely>0:
+                if j.rect.bottom > b.rect.top:
+                    j.rect.bottom = b.rect.top
+                    j.vely=0
+
+            if j.vely<0:
+                if j.rect.top < b.rect.bottom:
+                    j.rect.top = b.rect.bottom
+                    j.vely=0
+
+        jugadores.update()
+        pantalla.fill(NEGRO)
+        jugadores.draw(pantalla)
+        bloques.draw(pantalla)
+        pygame.display.flip()
+        reloj.tick(10)
---
+++ juego002.py
@@ -0,0 +1,103 @@
+import pygame 
+import random
+from grafica import *
+
+ANCHO = 640
+ALTO = 480
+
+class Jugador(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([40,50])
+        self.image.fill(VERDE)
+        self.rect = self.image.get_rect()
+        self.rect.x = 100
+        self.rect.y = 200
+        self.velx = 0
+        self.vely = 0
+
+    def update(self):
+        self.rect.x += self.velx
+        self.rect.y +=self.vely
+
+class Rival(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([40,50])
+        self.image.fill(ROSADO)
+        self.rect = self.image.get_rect()
+        self.rect.x = 100
+        self.rect.y = 200
+        self.velx = 0
+        self.vely = 0
+
+    def update(self):
+        self.rect.x += self.velx
+        self.rect.y +=self.vely
+
+
+if __name__ == '__main__':
+    #Seccion de variables
+
+    pygame.init()
+    pantalla = pygame.display.set_mode([ANCHO,ALTO])
+    jugadores = pygame.sprite.Group()
+
+    j = Jugador()
+    jugadores.add(j)
+    
+    rivales  = pygame.sprite.Group()
+    n = 10 
+    for i in range(n):
+        r = Rival()
+        r.rect.x = random.randrange(ANCHO)
+        r.rect.y = random.randrange(ALTO)
+        rivales.add(r)
+
+    reloj = pygame.time.Clock()
+    fin = False
+    #Ciclo principal
+    while not fin:
+        #Gestion de eventos
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                fin = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    j.velx += 5
+                    j.vely = 0
+                if event.key == pygame.K_LEFT:
+                    j.velx += -5
+                    j.vely = 0
+                if event.key == pygame.K_UP:
+                    j.velx = 0
+                    j.vely += 5
+                if event.key == pygame.K_DOWN:
+                    j.velx = 0
+                    j.vely += -5
+
+
+        #Gestion de control
+        if j.rect.x < 0:
+            j.rect.x = 0
+            j.velx=0
+        if j.rect.x > (ANCHO - j.rect.width):
+            j.rect.x = ANCHO - j.rect.width
+            j.velx=0
+        if j.rect.y < 0:
+            j.rect.y = 0
+            j.vely=0
+        if j.rect.y > (ALTO - j.rect.height):
+            j.rect.y = ALTO - j.rect.height
+            j.vely=0
+        
+        ls = pygame.sprite.spritecollide(j,rivales,True)
+        for e in ls:
+            print 'Colision'
+        #Gestion pantalla
+        jugadores.update()
+        pantalla.fill(NEGRO)
+        jugadores.draw(pantalla)
+        rivales.draw(pantalla)
+        pygame.display.flip()
+        reloj.tick(60)
